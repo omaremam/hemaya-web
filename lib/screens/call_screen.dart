6523388b456644dac4ce1dec66f3733cbea0b14b
AbdelrahmanAshraf9079 <abdelrahman9709@gmail.com>
2023-10-13 15:52:47 +0300

    2 way call implemented

--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:hemayaweb/screens/join_screen.dart';
@@ -10,6 +13,7 @@ class CallScreen extends StatefulWidget {
   final double lat, long;
   final String name;
   final dynamic offer;
+  final bool calling;
   const CallScreen({
     super.key,
     this.offer,
@@ -18,6 +22,7 @@ class CallScreen extends StatefulWidget {
     required this.lat,
     required this.long,
     required this.name,
+    required this.calling,
   });
 
   @override
@@ -25,11 +30,12 @@ class CallScreen extends StatefulWidget {
 }
 
 class _CallScreenState extends State<CallScreen> {
+  dynamic callOffer;
   // socket instance
   final socket = SignallingService.instance.socket;
 
   // videoRenderer for localPeer
-
+  final _localRTCVideoRenderer = RTCVideoRenderer();
   // videoRenderer for remotePeer
   final _remoteRTCVideoRenderer = RTCVideoRenderer();
 
@@ -44,8 +50,12 @@ class _CallScreenState extends State<CallScreen> {
 
   bool isCardVisible = false;
 
+  // media status
+  bool isAudioOn = true, isVideoOn = true, isFrontCameraSelected = true;
+
   @override
   void initState() {
+    callOffer = widget.offer;
     // initializing renderers
     print("${widget.offer} THIS IS THE OFFER");
 
@@ -104,7 +114,7 @@ class _CallScreenState extends State<CallScreen> {
     };
 
     // for Incoming call
-    if (widget.offer != null) {
+    if (widget.offer != null && !widget.calling) {
       // listen for Remote IceCandidate
       socket!.on("IceCandidate", (data) {
         String candidate = data["iceCandidate"]["candidate"];
@@ -141,8 +151,38 @@ class _CallScreenState extends State<CallScreen> {
       });
     }
     // for Outgoing Call
-    else {
-      // listen for local iceCandidate and add it to the list of IceCandidate
+    else if (widget.offer == null && widget.calling) {
+      // create peer connection
+      _rtcPeerConnection = await createPeerConnection({
+        'iceServers': [
+          {
+            'urls': [
+              'stun:stun1.l.google.com:19302',
+              'stun:stun2.l.google.com:19302'
+            ]
+          }
+        ]
+      });
+
+      // listen for remotePeer mediaTrack event
+      _rtcPeerConnection!.onTrack = (event) {
+        _remoteRTCVideoRenderer.srcObject = event.streams[0];
+        setState(() {});
+      };
+
+      // get localStream
+      _localStream = await navigator.mediaDevices
+          .getUserMedia({'audio': isAudioOn, 'video': isVideoOn});
+
+      // add mediaTrack to peerConnection
+      _localStream!.getTracks().forEach((track) {
+        _rtcPeerConnection!.addTrack(track, _localStream!);
+      });
+
+      // set source for local video renderer
+      _localRTCVideoRenderer.srcObject = _localStream;
+
+      // for Incoming call
       _rtcPeerConnection!.onIceCandidate =
           (RTCIceCandidate candidate) => rtcIceCadidates.add(candidate);
 
@@ -171,28 +211,104 @@ class _CallScreenState extends State<CallScreen> {
 
       // create SDP Offer
       RTCSessionDescription offer = await _rtcPeerConnection!.createOffer();
-
+      setState(() {
+        callOffer = offer;
+      });
       // set SDP offer as localDescription for peerConnection
       await _rtcPeerConnection!.setLocalDescription(offer);
 
       // make a call to remote peer over signalling
-      socket!.emit('makeCall', {
-        "calleeId": widget.calleeId,
+
+      socket!.emit('makeMobileCall', {
+        "callerId": widget.callerId,
         "sdpOffer": offer.toMap(),
+        "calleeEmail": widget.calleeId,
       });
     }
   }
 
-  _leaveCall() {
-    Navigator.pop(context);
+  _startMobileCall(String calleeEmail) async {
+    // Listen for local iceCandidate and add it to the list of IceCandidate
+    _rtcPeerConnection!.onIceCandidate =
+        (RTCIceCandidate candidate) => rtcIceCadidates.add(candidate);
 
-    // Set the state to show the new card component
+    // when call is accepted by the remote peer
+    socket!.on("callAnswered", (data) async {
+      // set SDP answer as remoteDescription for peerConnection
+      await _rtcPeerConnection!.setRemoteDescription(
+        RTCSessionDescription(
+          data["sdpAnswer"]["sdp"],
+          data["sdpAnswer"]["type"],
+        ),
+      );
+
+      // send iceCandidate generated to the remote peer over signalling
+      for (RTCIceCandidate candidate in rtcIceCadidates) {
+        socket!.emit("IceCandidate", {
+          "calleeId": calleeEmail,
+          "iceCandidate": {
+            "id": candidate.sdpMid,
+            "label": candidate.sdpMLineIndex,
+            "candidate": candidate.candidate
+          }
+        });
+      }
+    });
+
+    // Now, you want to initiate a call to the mobile app user
+    RTCSessionDescription offer = await _rtcPeerConnection!.createOffer();
+
+    // set SDP offer as localDescription for peerConnection
+    await _rtcPeerConnection!.setLocalDescription(offer);
+
+    // make a call to the mobile app user over signalling
+    socket!.emit('makeMobileCall', {
+      "calleeEmail": calleeEmail,
+      "sdpOffer": offer.toMap(),
+      "callerId": widget.callerId,
+    });
+  }
+
+  _leaveCall() {
+    // Navigator.pop(context);
+
+    // // Set the state to show the new card component
+    // Future.delayed(Duration.zero, () {
+    //   setState(() {
+    //     isCardVisible =
+    //         true; // Assuming isCardVisible is a boolean variable in your state
+    //   });
+    // });
+
     Future.delayed(Duration.zero, () {
       setState(() {
         isCardVisible =
             true; // Assuming isCardVisible is a boolean variable in your state
+        isAudioOn = false; // Turn off audio
+        isVideoOn = false; // Turn off video
       });
     });
+
+    // Stop the local video renderer
+    _localRTCVideoRenderer.srcObject = null;
+
+    // Stop the local media stream
+    _localStream?.getTracks().forEach((track) {
+      track.stop();
+    });
+
+    // If you want to dispose the local media stream, uncomment the following line
+    _localStream?.dispose();
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (_) => JoinScreen(
+          selfCallerId: "1234",
+          isCardVisible: true,
+        ),
+      ),
+    );
   }
 
   @override
@@ -218,58 +334,56 @@ class _CallScreenState extends State<CallScreen> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                alignment: Alignment.centerLeft,
-                width: MediaQuery.of(context).size.width * 0.7,
-                height: MediaQuery.of(context).size.height * 0.9,
-                child: RTCVideoView(
-                  _remoteRTCVideoRenderer,
-                  objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+              if (widget.offer != null || callOffer != null)
+                Container(
+                  alignment: Alignment.centerLeft,
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: RTCVideoView(
+                    _remoteRTCVideoRenderer,
+                    objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                  ),
                 ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [Text("Name"), Text("${widget.name}")],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [Text("Latitude"), Text("${widget.lat}")],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [Text("Longitude"), Text("${widget.long}")],
-                    ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            elevation: 1, backgroundColor: Color(0xFF1281AE)),
-                        onPressed: () {
-                          _openMaps();
-                        },
-                        child: Text("View Location")),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            elevation: 1, backgroundColor: Color(0xFF1281AE)),
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => JoinScreen(
-                                selfCallerId: "1234",
-                                isCardVisible: true,
-                              ),
-                            ),
-                          );
-                        },
-                        child: Text("End call")),
-                  ],
+              if (widget.offer != null || callOffer != null)
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [Text("Name"), Text("${widget.name}")],
+                      ),
+                      if (widget.offer != null)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [Text("Latitude"), Text("${widget.lat}")],
+                        ),
+                      if (widget.offer != null)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [Text("Longitude"), Text("${widget.long}")],
+                        ),
+                      if (widget.offer != null)
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                elevation: 1,
+                                backgroundColor: Color(0xFF1281AE)),
+                            onPressed: () {
+                              _openMaps();
+                            },
+                            child: Text("View Location")),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 1, backgroundColor: Color(0xFF1281AE)),
+                          onPressed: () {
+                            _leaveCall();
+                          },
+                          child: Text("End call")),
+                    ],
+                  ),
                 ),
-              ),
               if (isCardVisible)
                 AfterCallWidget(
                   switchToggle: onToggleSwitch,
--- lib/screens/join_screen.dart
+++ lib/screens/join_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:hemayaweb/widgets/after_call_card.dart';
@@ -20,16 +19,8 @@ class JoinScreen extends StatefulWidget {
 class _JoinScreenState extends State<JoinScreen> {
   dynamic incomingSDPOffer;
   bool toggle = false;
-  final remoteCallerIdTextEditingController = TextEditingController();
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  onToggleSwitch() {
-    setState(() => toggle = !toggle);
-  }
-
+  final emailController = TextEditingController();
+  onToggleSwitch() {}
   @override
   void initState() {
     super.initState();
@@ -37,7 +28,7 @@ class _JoinScreenState extends State<JoinScreen> {
     print("Incoming sdp offer");
     print(incomingSDPOffer);
 
-    document.documentElement?.requestFullscreen();
+    // document.documentElement?.requestFullscreen();
     // listen for incoming video call
     SignallingService.instance.socket!.on("newCall", (data) {
       print(data);
@@ -50,7 +41,24 @@ class _JoinScreenState extends State<JoinScreen> {
     });
   }
 
-  // join Call
+  _callUsers() async{
+    print("callscreen");
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (_) => CallScreen(
+          callerId: "1234",
+          calleeId: emailController.text,
+          offer: null,
+          lat: 0.0,
+          long: 0.0,
+          name: emailController.text,
+          calling: true,
+        ),
+      ),
+    );
+  }
+
   _joinCall({
     required String callerId,
     required String name,
@@ -69,6 +77,7 @@ class _JoinScreenState extends State<JoinScreen> {
           lat: latitude,
           long: longitude,
           name: name,
+          calling: false,
         ),
       ),
     );
@@ -79,13 +88,15 @@ class _JoinScreenState extends State<JoinScreen> {
     return Scaffold(
       appBar: AppBar(
         flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF009F98), Color(0xFF1281AE)],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
+          width: 100,
+          height: 40,
+          // decoration: const BoxDecoration(
+          //   gradient: LinearGradient(
+          //     colors: [Color(0xFF009F98), Color(0xFF1281AE)],
+          //     begin: Alignment.topCenter,
+          //     end: Alignment.bottomCenter,
+          //   ),
+          // ),
         ),
         centerTitle: true,
         title: const Text("Hemaya Server"),
@@ -98,9 +109,7 @@ class _JoinScreenState extends State<JoinScreen> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text(
-                      "Incoming Call",
-                    ),
+                    const Text("Incoming Call"),
                     IconButton(
                       icon: const Icon(Icons.call_end),
                       color: Colors.redAccent,
@@ -113,21 +122,65 @@ class _JoinScreenState extends State<JoinScreen> {
                       color: Colors.greenAccent,
                       onPressed: () {
                         _joinCall(
-                            callerId: incomingSDPOffer["callerId"]!,
-                            offer: incomingSDPOffer["sdpOffer"],
-                            name: incomingSDPOffer["name"],
-                            latitude: incomingSDPOffer["lat"],
-                            longitude: incomingSDPOffer["long"]);
+                          callerId: incomingSDPOffer["callerId"]!,
+                          offer: incomingSDPOffer["sdpOffer"],
+                          name: incomingSDPOffer["name"],
+                          latitude: incomingSDPOffer["lat"],
+                          longitude: incomingSDPOffer["long"],
+                        );
                       },
-                    )
+                    ),
                   ],
                 ),
               ),
             if (incomingSDPOffer == null)
-              const Center(child: Text("No incoming calls")),
+              Center(
+                child: Text("No incoming calls"),
+              ),
             if (toggle)
               Center(
                 child: AfterCallWidget(switchToggle: onToggleSwitch),
+              ),
+            if (incomingSDPOffer == null)
+              Positioned(
+                bottom: 5,
+                right: 5,
+                child: Container(
+                  constraints: BoxConstraints(
+                    maxWidth: 400.0,
+                    maxHeight: 100.0,
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Flexible(
+                          child: TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: "Enter email",
+                              // Add styling as needed
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 16.0),
+                        ElevatedButton(
+                          onPressed:() async{
+                            await _callUsers();
+                          },
+                          child: Text("Call"),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Color(0xFF009F98), // Background color
+                            foregroundColor: Colors.white, // Text color
+                          ),
+                          // Add styling as needed
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
           ],
         ),
